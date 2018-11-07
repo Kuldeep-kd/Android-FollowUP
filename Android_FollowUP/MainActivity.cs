9de4e847a6dd257c4bc9a7bc88355a756fce3646
Kuldeep Thakre <kuldeep-kd@live.com>
2018-11-07 14:53:59 +0530

    Working SignUp Event

--- Android_FollowUP/MainActivity.cs
+++ Android_FollowUP/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.OS;
 using Android.Support.V7.App;
 using System.Collections.Generic;
 using System;
+using System.Threading;
+using Android.Views;
 
 namespace Android_FollowUP
 {
@@ -14,6 +16,7 @@ namespace Android_FollowUP
 
         private List<Person> People;
         private ListView view;
+        private ProgressBar progress;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,22 +24,52 @@ namespace Android_FollowUP
 
             SetContentView(Resource.Layout.activity_main);
 
+            progress = FindViewById<ProgressBar>(Resource.Id.prgBar);
             view = FindViewById<ListView>(Resource.Id.lstView);
             People = new List<Person>();
 
+            //Lazy enough to populate the list...
             for(int x =1; x<10;x++)
-                People.Add(new Person() { FirstName = "Kuldeep"+x.ToString(), LastName = "Thakre" + x.ToString(), Age = 20+x, Gender = "Male" + x.ToString() });
+                People.Add(new Person() { FirstName = "Kuldeep"+x.ToString(), LastName = "Thakre" + x.ToString(), Age = 15+x, Gender = "Male" + x.ToString() });
+
 
             MyListViewAdapter adapter = new MyListViewAdapter(this, People);
 
             view.Adapter = adapter;
             view.ItemClick += View_ItemClick;
+            var btn = FindViewById<Button>(Resource.Id.btnSignUp);
+            btn.Click += Btn_Click;
+            
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            SignUpFragment fragment = new SignUpFragment();
+            fragment.Show(transaction,"transaction");
+            fragment.OnSignUpEventArgs += Fragment_OnSignUpEventArgs;
+        }
+
+        private void Fragment_OnSignUpEventArgs(object sender, SignUpEventArgs e)
+        {
+            Console.WriteLine(e.Name + " " + e.Email + " " + e.Password);
+            progress.Visibility = ViewStates.Visible;
+            Thread t = new Thread(Timerr);
+            t.Start();
+        }
+
+        private void Timerr()
+        {
+            Thread.Sleep(3000);
+
+            RunOnUiThread(() => { progress.Visibility = ViewStates.Invisible; });
         }
 
         private void View_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
             Console.WriteLine(People[e.Position].FirstName);
         }
+
     }
 }
 
---
+++ Android_FollowUP/SignUpFragment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Android_FollowUP
+{
+
+    public class SignUpEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public SignUpEventArgs(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+
+    
+
+    public class SignUpFragment : DialogFragment
+    {
+        private EditText name;
+        private EditText email;
+        private EditText password;
+        private Button BtnSignUp;
+        public event EventHandler<SignUpEventArgs> OnSignUpEventArgs;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            View view = inflater.Inflate(Resource.Layout.sign_in_box, container, false);
+            name = view.FindViewById<EditText>(Resource.Id.Name);
+            email = view.FindViewById<EditText>(Resource.Id.EMail);
+            password = view.FindViewById<EditText>(Resource.Id.Password);
+            BtnSignUp = view.FindViewById<Button>(Resource.Id.btnFrgSignUp);
+
+            BtnSignUp.Click += BtnSignUp_Click;
+
+            // Use this to return your custom view for this Fragment
+            return view;
+        }
+
+        private void BtnSignUp_Click(object sender, EventArgs e)
+        {
+            OnSignUpEventArgs.Invoke(this, new SignUpEventArgs(name.Text, email.Text, password.Text));
+            this.Dismiss();
+        }
+    }
+}
